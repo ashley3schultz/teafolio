9cfec99c226bb6e5523ca01be778e27f11507271
ashley3schultz <ashleyeschultz@gmail.com>
2018-07-15 12:44:59 -0700

    add helpers to tea controller

--- app/controllers/teas_controller.rb
+++ app/controllers/teas_controller.rb
@@ -1,9 +1,17 @@
 class TeasController < ApplicationController
+    before_action :find_tea, only: [:show, :update, :edit, :destroy]
 
     def index 
+        @teas = Tea.all
     end 
 
+    def new
+        @tea = Tea.new
+    end
+
     def create 
+        @tea = Tea.new 
+
     end
 
     def show
@@ -17,5 +25,15 @@ class TeasController < ApplicationController
 
     def destroy
     end 
+
+    def find_tea
+        @tea = Tea.find(params[:id])
+    end 
+
+    private 
+
+        def tea_params
+            params.require(:tea).permit(:name, :aka, :oxidation, :description)
+        end 
     
 end
