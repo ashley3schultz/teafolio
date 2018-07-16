df49c3686381cb64ccad7b4d8ff54703b3bc9e19
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-16 19:45:22 +0000

    add helper methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
 
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def valid_owner?(obj)
+    obj.user_id == current_user.id
+  end
 
 end
