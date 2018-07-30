0223f43dbc0ab90485a3d980c5067d3862f3c264
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-30 16:00:48 +0000

    update custom validation

--- app/models/pending_tea.rb
+++ app/models/pending_tea.rb
@@ -9,7 +9,7 @@ class PendingTea < ApplicationRecord
       parr = PendingTea.all.pluck(:name, :aka)
       parr.each do |a|
         a.each do |n|
-          teas << n.downcase.gsub(" ","") unless n.nil?
+          teas << n.downcase.gsub(" ","") unless n.blank?
         end
       end
       tarr = Tea.all.pluck(:name, :aka)
